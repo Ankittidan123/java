a03b280fb245b0d8af7510b7aad41c9a2406fecb
ankit jangir <ankitjangir8764064082@gmail.com>
2024-02-05 13:44:18 +0530

    basic assginment done

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main{
+    public static void main(String[] args){
+        int a=5, b=2, e=89, f=2, g=34;
+        float c=8.9f, d=1.2f;
+        float h=a*b+c*d-e/f+g;
+        System.out.println(h+"\n");
+    }
+}
