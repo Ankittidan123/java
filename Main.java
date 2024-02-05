ba180b1a9d170b12602bac25ad927e4998c433ad
ankit jangir <ankitjangir8764064082@gmail.com>
2024-02-05 23:17:31 +0530

    user input assginment done

--- Main.java
+++ Main.java
@@ -1,8 +1,9 @@
+import java.util.*;
 public class Main{
     public static void main(String[] args){
-        int a=5, b=2, e=89, f=2, g=34;
-        float c=8.9f, d=1.2f;
-        float h=a*b+c*d-e/f+g;
-        System.out.println(h+"\n");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the String");
+        String name = sc.next();
+        System.out.println("Kajal is a trainer");
     }
 }
