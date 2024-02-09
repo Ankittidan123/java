1fadb5725f834b669299641b6478dabf525155d6
ankit jangir <ankitjangir8764064082@gmail.com>
2024-02-09 13:37:03 +0530

    assginment done

--- Main.java
+++ Main.java
@@ -1,57 +1,16 @@
 import java.util.*;
+ 
 public class Main {
-
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        System.out.println("enter area/vol");
-        String al=sc.nextLine();
-        if(al.equals("area")){
-            System.out.println("enter the character");
-            String alpha=sc.nextLine();
-            if(alpha.equals("tringle")){
-                System.out.println("enter the height");
-                int h=sc.nextInt();
-                System.out.println("enter the base");
-                int b=sc.nextInt();
-                System.out.println("the area of tringle is"+((b*h)/2));
-            }
-            else if(alpha.equals("square")){
-                System.out.println("enter the side value");
-                int l=sc.nextInt();
-                System.out.println("the area of square is "+(b*b));
-            }
-            else if(alpha.equals("rectangle")){
-                System.out.println("enter the length");
-                int l=sc.nextInt();
-                System.out.println("enter the width");
-                int w=sc.nextInt();
-                System.out.println("the area of rectangle is"+(l*w));
-            }
-        }
-        else if(al.equals("volumn")){
-            System.out.println("Enter the char");
-            String alpha = sc.nextLine();
-            if(alpha.equals("triangle")){
-        System.out.println("enter the height");
-        int h=sc.nextInt();
-        System.out.println("enter the base");
-        int b=sc.nextInt();
-        System.out.println("the volume of tringle is"+((b*h)/2));
+    public static void main(String[] args)
+    {
+      Scanner sc=new Scanner(System.in);
+      System.out.println("enter the four digit number :");
+      int a=sc.nextInt();
+      if(a%3==0 || a%5==0 || a%7==0){
+        System.out.print("this is lucky number :"+a);
     }
-    else if(alpha.equals("square")){
-        System.out.println("enter the side value");
-        int b=sc.nextInt();
-        System.out.println("the volume of square is "+(b*b*b));
+    else{
+        System.out.println("invalid number"+a);
     }
-    else if(alpha.equals("rectangle")){
-        System.out.println("enter the length");
-        int l=sc.nextInt();
-        System.out.println("enter the width");
-        int w=sc.nextInt();
-        System.out.println("enter the breath");
-        int b=sc.nextInt();
-        System.out.println("the volume of rectangle is"+(l*w*b));
-    }
-}
     }
 }
