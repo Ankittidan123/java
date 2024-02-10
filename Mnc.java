ffb6406f2fc160afe03c676dcc18f2003ca08956
ankit jangir <ankitjangir8764064082@gmail.com>
2024-02-10 11:38:53 +0530

    assginment in progress

--- Mnc.java
+++ Mnc.java
@@ -1,27 +1,14 @@
 import java.util.*;
-
-public class Mnc {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number for car");
-        int num = sc.nextInt();
-        if(num>=1000 && num<=9999){
-            int rem;
-            int sum = 0;
-            while(num>0){
-                rem = num%10;
-                sum = sum+rem;
-                num = num/10;
-            }
-            if(sum%3==0 || sum%5==0 || sum%7==0){
-                System.out.println("This is lucky number");
-            }
-            else{
-                System.out.println("This is not lucky number");
-            }
+ 
+public class Main {
+    public static void main(String[] args)
+    {
+       int a=1;
+       for(int i=4; i>=1; i--){
+        for(int j=1; j<=4; j++){
+            System.out.print(a++);
         }
-        else{
-            System.out.println("Not vaild input");
-        }
+        System.out.println();
+       }
     }
 }
