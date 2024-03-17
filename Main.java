6bcb738c9405f91fe396bfa2c99161da50b45f36
ankit jangir <ankitjangir8764064082@gmail.com>
2024-03-17 20:32:13 +0530

    assginment done

--- Main.java
+++ Main.java
@@ -1,15 +1,41 @@
 import java.util.*;
- 
-public class Main {
-    public static void main(String[] args)
-    {
-       int a=1;
-       for(int i=4; i>=1; i--){
-        for(int j=1; j<=4; j++){
-            System.out.print(a++);
-        }
+public class Main{
+void add(int a, int b){
+    int c=a+b;
+    System.out.println("Sum of " + a+ " and " + b + " is: " +c);
+
+}
+void sub(int a, int b){
+    int c=a-b;
+    System.out.println("sub of " + a+ " and " + b + " is: " +c);
+}
+void div(int a,int b){
+    int c=a/b;
+    System.out.println("div of " + a+ " and " + b + " is: " +c);
+}
+void multi(int a,int b){
+    int c=a*b;
+    System.out.println("multi of " + a+ " and " + b + " is: " +c);
+}
+void mod(int a,int b){
+    int c=a%b;
+    System.out.println("mod of " + a+ " and " + b + " is: " +c);
+}
+
+public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("enter the value of a and b :");
+    int a=sc.nextInt();
+    int b=sc.nextInt();
+
+    Main aj=new Main();
+    aj.add(a, b);
+    aj.sub(a, b);
+    aj.div(a, b);
+    aj.multi(a, b);
+    aj.mod(a, b);
     
-        System.out.println();
-       }
-    }
+    
+}
+
 }
--- Mnc.java
+++ Mnc.java
@@ -1,24 +1,39 @@
 import java.util.*;
  
 public class Mnc{
-  void swap(){
-  Scanner sc=new Scanner(System.in);
-  System.out.println("enter the value of a :");
-  int a=sc.nextInt();
-  System.out.println("enter the value of b:");
-  int b=sc.nextInt();
-  int c;
-  c=a;
-  a=b;
-  b=c; 
-  System.out.println("the swap value of a :"+a);
-  System.out.println("the swap value of b :"+b); 
-  }  
-  void pallindrome(){
-    
-  }
+
+       public static void upper(String str){
+          StringBuilder sb = new StringBuilder("");
+            char ch = Character.toUpperCase(str.charAt(0));
+            sb.append(ch);
+            for(int i=1; i<str.length(); i++){
+              if(str.charAt(i)==' ' && i<str.length()-1){
+                sb.append(str.charAt(i));
+                i++;
+                sb.append(Character.toUpperCase(str.charAt(i)));
+              }
+              else{
+                sb.append(str.charAt(i));
+              }
+            }
+          System.out.println(sb.toString());
+          StringBuilder sb1 = new StringBuilder("");
+          char chh = Character.toLowerCase(sb.toString().charAt(0));
+          sb1.append(chh);
+          for(int i=1; i<sb.toString().length(); i++){
+            if(sb.toString().charAt(i)==' ' && i<sb.toString().length()-1){
+              sb1.append(sb.toString().charAt(i));
+              i++;
+              sb1.append(Character.toLowerCase(sb.toString().charAt(i)));
+            }
+            else{
+              sb1.append(Character.toUpperCase(sb.toString().charAt(i)));
+            }
+          }
+          System.out.println(sb1.toString());
+        }
   public static void main(String[] args) {
-    Mnc k=new Mnc();
-    k.swap();
+    String str = "hello, ankit this side";
+    upper(str);
   }
 }
