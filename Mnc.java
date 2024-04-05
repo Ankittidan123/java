b3fae5499544adf3eb7de76db943275ace39bdf7
ankit jangir <ankitjangir8764064082@gmail.com>
2024-04-05 10:27:42 +0530

    assginment done

--- Mnc.java
+++ Mnc.java
@@ -1,30 +1,12 @@
 import java.util.Scanner;
 class A{
-    void show(int a,int b){
-        int c = a+b;
-        System.out.println(c);
-    }
-    void show(int a,int b){
-        int c = a-b;
-        System.out.println(c);
-    }
-    void show(int a,int b) {
-        int c = a/b;
-        System.out.println(c);
-    }
-    void show(int a,int b){
-        int c = a*b;
-        System.out.println(c);
+    protected void show(){
+        System.out.println("Hello i am show of A");
     }
 }
-public class Mnc{
+public class Mnc extends A{
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number");
-        int a = sc.nextInt();
-        System.out.println("Enter the number");
-        int b = sc.nextInt();
-        A k2 = new A();
-        k2.show(a,b);
+
     }
 }
--- Test.java
+++ Test.java
@@ -1,18 +1,25 @@
 import java.util.*;
- 
-public class Test {
-    public static void main(String[] args)
-    {
-       for(int i=0;i<=6;i++){
-        int num=i%2==0?1:0;
-
-       for(int j=0;j<=i;j++){
-        System.out.print(" "+num);
-        num = ( num+1 )%2;
-       }
-       System.out.println();
-
-        }
+final class A{
+    final int a = 2;
+    void setA(int b){
+        b=a;
+    }
+    int getA(){
+        return a;
+    }
+    int show(){
+        System.out.println("Hello this is a "+a);
+        return a;
     }
 }
 
+public class Test{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value");
+        int a = sc.nextInt();
+        A k = new A();
+        k.show();
+        k.setA(a);
+    }
+}
