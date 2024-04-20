79c42973fcc2a330dcb87eea426a496e151128f7
ankit jangir <ankitjangir8764064082@gmail.com>
2024-04-20 08:58:13 +0530

    assginment done

--- Main.java
+++ Main.java
@@ -1,26 +1,117 @@
-import java.util.*;
 
-class A {
-    
-    int a;
-    int b;
-    A(int a, int b) {
-        // this.a = a;
-        // this.b = b; 
+
+
+class A{
+    int c;
+
+    A(int c){
+        this.c=c;
+        System.out.println(c);
+
     }
 
-    void method1(int a,int b) {
-        this.a=a;
-        this.b=b;
-        int sum=a+b;
-        System.out.println(sum);
+    int method1(){
+        this.c=c;
+        System.out.println("c value "+ c);
+        return c;
+
+    }
+    
+}
+class B{
+    int d;
+
+    B(int d ){
+        this.d=d;
+        System.out.println(d);
+
+    }
+
+    int method2(int d ){
+        this.d=d;
+        System.out.println("Value is "+ d);
+        return d;
+    }
+}
+class C{
+    int g;
+    C(int g){
+        this.g=g;
+        System.out.println("value is " + g);
+
+    }
+    void method3(){
+        this.g=g;
+        System.out.println("value is "+g);
+    }
+
+
+}
+class D{
+    int j;
+    D(int j){
+        this.j = j;
+        System.out.println(j);
+
+    }
+
+    int method4(){
+        this.j=j;
+        System.out.println("Value is "+j);
+        return j;
+    }
+}
+class E{
+    int m;
+    E(int m){
+        this.m=m;
+        System.out.println(m);
+    }
+
+    void method4(){
+        this.m=m;
+        System.out.println("Value of  "+m);
     }
 }
 
 public class Main {
 
+
     public static void main(String[] args) {
-        A k = new A(0, 0);
-        k.method1(4, 6);
+
+        int a=8;
+        int b=9;
+        int c = a+b;
+        
+        A aj = new A(c);
+         aj.method1();
+         int e = 5;
+         int f = 4;
+ 
+         int d= e-f;
+ 
+         B bj= new B(d);
+         bj.method2(d);
+         int h = 6;
+        int i = 4;
+
+        int g = h*i;
+
+        C cj = new C(g);
+        cj.method3();
+        int k = 6;
+        int l = 2;
+
+        int j = k/l;
+
+        D dj = new D(j);
+        dj.method4();
+        int n= 25;
+        int o = 5;
+
+        int m = n%o;
+
+        E ej = new E(m);
+        ej.method4();
     }
 }
--- Mnc.java
+++ Mnc.java
@@ -1,12 +1,30 @@
+import java.util.*;
+class A{
+    Scanner sc=new Scanner(System.in);
+       void getA(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the String");
+        String name = sc.next();
+        System.out.println("empname ");
+        String na=sc.nextLine();
+        System.out.println("emf");
+        String nam=sc.nextLine();
+        System.out.println(" salary :");
+        int a=sc.nextInt();
+
+       }
+       int setA(void getA){
+    
+    System.out.println(getA());
+       return getA;
+    }
 public class Mnc{
-    int show(){
-        int a=1;
-        int b=2;
-        int c=a+b;
-        return c;
+
+    public static void main(String [] args)
+    {
+        Mnc k=new Mnc();
+        k.setA();
+      System.out.println(k.getA());
     }
-    public static void main(String[] args) {
-        Main k=new Main();
-        System.out.println(k.show());
-    }
+}
 }
