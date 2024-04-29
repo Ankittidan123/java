6067f2dc69808cf902d36b5e16c4519645293447
ankit jangir <ankitjangir8764064082@gmail.com>
2024-04-29 12:30:59 +0530

    assginment done

--- Main.java
+++ Main.java
@@ -1,117 +1,92 @@
+import java.util.*;
+//abstract class and abstract method : 
 
-
-
-class A{
-    int c;
-
-    A(int c){
-        this.c=c;
-        System.out.println(c);
-
-    }
-
-    int method1(){
-        this.c=c;
-        System.out.println("c value "+ c);
-        return c;
-
-    }
-    
-}
-class B{
-    int d;
-
-    B(int d ){
-        this.d=d;
-        System.out.println(d);
-
-    }
-
-    int method2(int d ){
-        this.d=d;
-        System.out.println("Value is "+ d);
-        return d;
-    }
-}
-class C{
-    int g;
-    C(int g){
-        this.g=g;
-        System.out.println("value is " + g);
-
-    }
-    void method3(){
-        this.g=g;
-        System.out.println("value is "+g);
-    }
-
-
-}
-class D{
-    int j;
-    D(int j){
-        this.j = j;
-        System.out.println(j);
-
-    }
-
-    int method4(){
-        this.j=j;
-        System.out.println("Value is "+j);
-        return j;
-    }
-}
-class E{
-    int m;
-    E(int m){
-        this.m=m;
-        System.out.println(m);
-    }
-
-    void method4(){
-        this.m=m;
-        System.out.println("Value of  "+m);
-    }
+abstract class Mahindra {
+    abstract void logo(); 
+   abstract void mirror();
+   abstract void ac();
+    abstract void airbag();
 }
 
-public class Main {
 
 
+abstract class Thar extends Mahindra {
+    void method(){
+        System.out.println("1st car");
+    }
+    void logo() {
+        System.out.println("attractive");
+    }
+    void mirror(){
+        System.out.println("2 mirror");
+    }
+    void ac(){
+        System.out.println("high ac");
+    }
+    void airbag(){
+        System.out.println("4 airbags");
+    }
+
+}
+abstract class Honda extends Thar {
+    void method1(){
+        System.out.println("2st car");
+    }
+    void method(){
+        System.out.println("1st car");
+    }
+    void logo() {
+        System.out.println("attractive");
+    }
+    void mirror(){
+        System.out.println("2 mirror");
+    }
+    void ac(){
+        System.out.println("high ac");
+    }
+    void airbag(){
+        System.out.println("4 airbags");
+    }
+
+
+}
+abstract class Hero extends Honda{
+    void method2(){
+        System.out.println("3rd car");
+    }
+    void method1(){
+        System.out.println("2st car");
+    }
+    void method(){
+        System.out.println("1st car");
+    }
+    void logo() {
+        System.out.println("attractive");
+    }
+    void mirror(){
+        System.out.println("2 mirror");
+    }
+    void ac(){
+        System.out.println("high ac");
+    }
+    void airbag(){
+        System.out.println("4 airbags");
+    }
+
+
+
+}
+
+public class Main extends  Hero {
     public static void main(String[] args) {
-
-        int a=8;
-        int b=9;
-        int c = a+b;
-        
-        A aj = new A(c);
-         aj.method1();
-         int e = 5;
-         int f = 4;
- 
-         int d= e-f;
- 
-         B bj= new B(d);
-         bj.method2(d);
-         int h = 6;
-        int i = 4;
-
-        int g = h*i;
-
-        C cj = new C(g);
-        cj.method3();
-        int k = 6;
-        int l = 2;
-
-        int j = k/l;
-
-        D dj = new D(j);
-        dj.method4();
-        int n= 25;
-        int o = 5;
-
-        int m = n%o;
-
-        E ej = new E(m);
-        ej.method4();
+    
+        Main t = new Main();
+        t.logo();
+        t.mirror();
+        t.ac();
+        t.airbag();
+        t.method();
+        t.method1();
+        t.method2();
     }
 }
--- Test.java
+++ Test.java
@@ -1,21 +1,54 @@
 import java.util.*;
- 
+
+abstract class A{
+    abstract int show1();
+    int  show2(int a,int b){
+       
+       int c=a*b;
+       System.out.println("multi :"+c);
+       return c;
+    }
+}
+class B extends A{
+
+    int show1(){
+        Scanner input=new Scanner(System.in);
+        int a=input.nextInt();
+        int b=input.nextInt();
+        int c=a+b;
+        System.out.println("addtion :"+c);
+        return c;
+    }
+    int show3(int d,int f){
+        
+       int e=d-f;
+       System.out.println("sub :"+e);
+       return e;
+    }
+    void show4(){
+        Scanner scan=new Scanner(System.in);
+        int a=scan.nextInt();
+        int b=scan.nextInt();
+        int c=a/b;
+        System.out.println("div"+c);
+
+    }
+}
 public class Test {
     public static void main(String[] args)
     {
-        for (int i=1; i<=6; i++) {
-            for(int s=5;s>=i;s--){
-                System.out.print(" ");
-            }
-            for(int j=1;j<=i;j++){
-                if((j)%2==0){
-                    System.out.print("0");
-                }
-                else{
-                    System.out.print("1");
-                }
-            }
-            System.out.println();
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+       
+        Scanner sc=new Scanner(System.in);
+        int d=sc.nextInt();
+        int f=sc.nextInt();
+
+     B k=new B();
+     k.show1();
+     k.show2(a, b);
+     k.show3(d, f);
+     k.show4();
                 }
     }
-}
