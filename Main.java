70b3d9fd3fc231b88c2f83e5519fbe8e0c953226
ankit jangir <ankitjangir8764064082@gmail.com>
2024-05-24 11:16:26 +0530

    assginment done

--- Main.java
+++ Main.java
@@ -1,92 +1,17 @@
-import java.util.*;
-//abstract class and abstract method : 
-
-abstract class Mahindra {
-    abstract void logo(); 
-   abstract void mirror();
-   abstract void ac();
-    abstract void airbag();
-}
-
-
-
-abstract class Thar extends Mahindra {
-    void method(){
-        System.out.println("1st car");
+public class Main {
+    static double[] show(double a[]){
+        return a;
     }
-    void logo() {
-        System.out.println("attractive");
+    static void display(double a[]){
+        for(int  i=0;i<a.length;i++){
+            System.out.print(a[i]+" ");
+        }
+        
     }
-    void mirror(){
-        System.out.println("2 mirror");
+    public static void main(String[] args){
+        double a[]={1.1, 2.2, 3.3, 4.4, 5.5};
+        double b[]=show(a);
+        display(b);
     }
-    void ac(){
-        System.out.println("high ac");
-    }
-    void airbag(){
-        System.out.println("4 airbags");
-    }
-
-}
-abstract class Honda extends Thar {
-    void method1(){
-        System.out.println("2st car");
-    }
-    void method(){
-        System.out.println("1st car");
-    }
-    void logo() {
-        System.out.println("attractive");
-    }
-    void mirror(){
-        System.out.println("2 mirror");
-    }
-    void ac(){
-        System.out.println("high ac");
-    }
-    void airbag(){
-        System.out.println("4 airbags");
-    }
-
-
-}
-abstract class Hero extends Honda{
-    void method2(){
-        System.out.println("3rd car");
-    }
-    void method1(){
-        System.out.println("2st car");
-    }
-    void method(){
-        System.out.println("1st car");
-    }
-    void logo() {
-        System.out.println("attractive");
-    }
-    void mirror(){
-        System.out.println("2 mirror");
-    }
-    void ac(){
-        System.out.println("high ac");
-    }
-    void airbag(){
-        System.out.println("4 airbags");
-    }
-
-
-
-}
-
-public class Main extends  Hero {
-    public static void main(String[] args) {
-    
-        Main t = new Main();
-        t.logo();
-        t.mirror();
-        t.ac();
-        t.airbag();
-        t.method();
-        t.method1();
-        t.method2();
-    }
-}
+       
+   }
--- Test.java
+++ Test.java
@@ -1,54 +1,28 @@
 import java.util.*;
 
-abstract class A{
-    abstract int show1();
-    int  show2(int a,int b){
-       
-       int c=a*b;
-       System.out.println("multi :"+c);
-       return c;
+public class Test{
+    public static int binarysearch(int number[], int key){
+        int start =0,end =number.length-1;
+        while(start<=end){
+            int mid=(start+end)/2;
+
+            //comparison
+            if(number[mid]==key){ //found
+                return mid;
+
+            }
+            if(number[mid]<key){ //right
+            start=mid+1;
+
+            }else{
+                end=mid-1;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+       int number[]={2,4,6,8,10,12,14,26};
+       int key =12 ;
+       System.out.println("index for key is :" +binarysearch(number, key));
     }
 }
-class B extends A{
-
-    int show1(){
-        Scanner input=new Scanner(System.in);
-        int a=input.nextInt();
-        int b=input.nextInt();
-        int c=a+b;
-        System.out.println("addtion :"+c);
-        return c;
-    }
-    int show3(int d,int f){
-        
-       int e=d-f;
-       System.out.println("sub :"+e);
-       return e;
-    }
-    void show4(){
-        Scanner scan=new Scanner(System.in);
-        int a=scan.nextInt();
-        int b=scan.nextInt();
-        int c=a/b;
-        System.out.println("div"+c);
-
-    }
-}
-public class Test {
-    public static void main(String[] args)
-    {
-        Scanner sc=new Scanner(System.in);
-        int a=sc.nextInt();
-        int b=sc.nextInt();
-       
-        Scanner sc=new Scanner(System.in);
-        int d=sc.nextInt();
-        int f=sc.nextInt();
-
-     B k=new B();
-     k.show1();
-     k.show2(a, b);
-     k.show3(d, f);
-     k.show4();
-                }
-    }
