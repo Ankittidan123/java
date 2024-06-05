6ed1acc67a3701e2f81d39065b37bc6552635eb7
ankit jangir <ankitjangir8764064082@gmail.com>
2024-06-05 16:05:49 +0530

    assginment done

--- Main.java
+++ Main.java
@@ -1,19 +1,43 @@
-import java.util.*;
-
-public class Main{
-    
+public class SwapEvenOdd {
     public static void main(String[] args) {
-        int a[] = {1,2,3,4,5,6,7,8};
-        int b[] = a.clone();
-        int c[] = b.clone();
-
-        int d[] = c.clone();
+        int[] a = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         
-        a[2] = 98;
-        a[4] = 56;
-        for(int i=0; i<d.length; i++){
-            
-            System.out.print(d[i]+" ");
+        // Print the original array
+        System.out.println("Original array:");
+        for (int i : a) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        
+        // Separate even and odd elements
+        int[] even = new int[a.length];
+        int[] odd = new int[a.length];
+        int evenCount = 0;
+        int oddCount = 0;
+        
+        for (int i : a) {
+            if (i % 2 == 0) {
+                even[evenCount++] = i;
+            } else {
+                odd[oddCount++] = i;
+            }
+        }
+        
+        // Merge even and odd elements back into the original array
+        int index = 0;
+        
+        for (int i = 0; i < evenCount; i++) {
+            a[index++] = even[i];
+        }
+        
+        for (int i = 0; i < oddCount; i++) {
+            a[index++] = odd[i];
+        }
+        
+        // Print the modified array
+        System.out.println("Modified array:");
+        for (int i : a) {
+            System.out.print(i + " ");
         }
     }
 }
--- Test.java
+++ Test.java
@@ -1,14 +1,54 @@
-public class Tes {
-    public static void target(int a[], int target) {
-        for (int i = 0; i < a.length; i++) {
-            if (a[i] == target) {
-                System.out.println("present");
-            }
-        }
+class A {
+    // default constructor
+    A() {
+        this(34);
+        System.out.println("default constructor ");
     }
+
+    // constructor with int parameter
+    A(int a) {
+        this(3.5f);
+        System.out.println("constructor with int: " + a);
+    }
+
+    // constructor with float parameter
+    A(float b) {
+        this('j');
+        System.out.println("constructor with float: " + b);
+    }
+
+    // constructor with char parameter
+    A(char c) {
+        this("ankit");
+        System.out.println("constructor with char: " + c);
+    }
+
+    // constructor with String parameter
+    A(String name) {
+        this((short) 32453);  // explicitly cast to short
+        System.out.println("constructor with String: " + name);
+    }
+
+    // constructor with short parameter
+    A(short d) {
+        this(462378546L);  // use a long literal
+        System.out.println("constructor with short: " + d);
+    }
+
+    // constructor with long parameter
+    A(long e) {
+        this(34.5);
+        System.out.println("constructor with long: " + e);
+    }
+
+    // constructor with double parameter
+    A(double f) {
+        System.out.println("constructor with double: " + f);
+    }
+}
+
+public class Main {
     public static void main(String[] args) {
-        int a[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-        int target = 8;
-        target(a, 8);
+        new A();
     }
 }
