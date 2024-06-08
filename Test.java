46c2b8ab581782d5d4da099241bd2c0756231789
ankit jangir <ankitjangir8764064082@gmail.com>
2024-06-08 10:46:09 +0530

    assginment done

--- Test.java
+++ Test.java
@@ -1,54 +1,23 @@
-class A {
-    // default constructor
-    A() {
-        this(34);
-        System.out.println("default constructor ");
-    }
-
-    // constructor with int parameter
-    A(int a) {
-        this(3.5f);
-        System.out.println("constructor with int: " + a);
-    }
-
-    // constructor with float parameter
-    A(float b) {
-        this('j');
-        System.out.println("constructor with float: " + b);
-    }
-
-    // constructor with char parameter
-    A(char c) {
-        this("ankit");
-        System.out.println("constructor with char: " + c);
-    }
-
-    // constructor with String parameter
-    A(String name) {
-        this((short) 32453);  // explicitly cast to short
-        System.out.println("constructor with String: " + name);
-    }
-
-    // constructor with short parameter
-    A(short d) {
-        this(462378546L);  // use a long literal
-        System.out.println("constructor with short: " + d);
-    }
-
-    // constructor with long parameter
-    A(long e) {
-        this(34.5);
-        System.out.println("constructor with long: " + e);
-    }
-
-    // constructor with double parameter
-    A(double f) {
-        System.out.println("constructor with double: " + f);
-    }
-}
-
-public class Main {
+public class Test {
     public static void main(String[] args) {
-        new A();
+          
+        for(int i=1; i<=5; i++){
+            for(int s=4; s>=i; s--){
+                System.out.print(" ");
+            }
+            for(int j=1; j<=i; j++){
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < 4; i++) {
+            for (int s = 0; s < 5-3/2; s++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 3; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
